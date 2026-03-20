c6858ce74236918b838019440b35f4c80651bf98
DominicDev <s0l0estudi4r@gmail.com>
2026-03-20 17:26:08 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'RIO';
+export let nameSongs = [
+	"LO PEOR DE TODO", /*1*/
+	"CONTÉSTAME", /*2*/
+	"LA UNIVERSIDAD", /*3*/
+	"AMORES QUE ACABAN", /*4*/
+	"TÍMIDO", /*5*/
+	"ABANDONADO", /*6*/
+	"SON COLEGIALAS", /*7*/
+	"ESTÁS EN NADA", /*8*/
+	"UN VIAJE EN UN MICRO", /*9*/
+	"LEJOS DE TI", /*10*/
+	"NO QUIERO VERTE MÁS", /*11*/
+	"LA EMPIEZO A ODIAR", /*12*/
+	"TODO ESTABA BIEN", /*13*/
+	"HUIR DE TI", /*14*/
+	"MI PARTIDO LO HARÁ", /*15*/
+	"PROFESORA DE INGLÉS", /*16*/
+	"AMOR ES", /*17*/
+	"REVOLUCIÓN", /*18*/
+	"PIERDO LA RAZÓN", /*19*/
+	"A LA DROGA DILE NO", /*20*/
+]
